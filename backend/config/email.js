348781644fe44d6412fa9e671c058554931b2420
mahesh18-hub <maheshburla562@gmail.com>
2025-06-30 15:40:52 +0530

    Update email.js

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,11 +1,11 @@
-// email.js content goes here
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "c.sec.balls@gmail.com",
-    pass: "wuim utok aaxv rbwk"
+        user: "c.sec.balls@gmail.com",
+        pass: "wuim utok aaxv rbwk"
   }
 });
 
