dc3a3803e39921b4eed7bb82c189b0249c574f10
mahesh18-hub <maheshburla562@gmail.com>
2025-06-30 15:22:01 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,9 @@
-// User.js content goes here
-import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, required: true, unique: true },
   password: String,
   isVerified: { type: Boolean, default: false },
   verifyToken: String,
   verifyTokenExpiry: Date,
   resetToken: String,
-  tokenExpiry: Date
+  tokenExpiry: Date,
 });
-
-export default mongoose.model("User", userSchema);
